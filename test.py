40bae7918bd7ecc746fbff70c3d8a1ea5441fd31
p81sunshine <lsnfzj@gmail.com>
2023-09-26 16:37:05 +0800

    Test which outof range

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
 from setup_imagenet_hash import ImageNet, ImageNet_HashModel
 
-data = ImageNet(0, 5, "../../../autodl-fs/n01983481" )
-print(data.file_list)
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-path", "--path", type=str)
+    args = vars(parser.parse_args())
+    data = ImageNet(0,5, args["path"])
+    print(data.file_list)
+    try:
+        item = data.file_list[4]
+    except IndexError:
+        print(args["path"])
